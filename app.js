8fc50dd23ad87c4763cd802dcfbb1646ad46005c
Dharmi Javiya <92503014+Dharmi7303@users.noreply.github.com>
2024-06-10 10:05:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -73,5 +73,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-   console.log(`🎯 Server is running on PORT: ${PORT}`);
+   console.log(`🎯 Server is running on PORT : ${PORT}`);
 });
